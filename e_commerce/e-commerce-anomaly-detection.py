1880ff81f591174f5cff13b0db5ff14c994cf6b8
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-07-24 10:21:54 +0200

    change format

---
+++ e_commerce/e-commerce-anomaly-detection.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+# +
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the read-only "../input/" directory
+# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
+
+import os
+for dirname, _, filenames in os.walk('./data'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
+# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
+# -
+
+customers = pd.read_csv("./data/olist_customers_dataset.csv")
+sellers = pd.read_csv("./data/olist_sellers_dataset.csv")
+reviews = pd.read_csv("./data/olist_order_reviews_dataset.csv")
+items = pd.read_csv("./data/olist_order_items_dataset.csv")
+products = pd.read_csv("./data/olist_products_dataset.csv")
+geolocation = pd.read_csv("./data/olist_geolocation_dataset.csv")
+category_name_translation = pd.read_csv("./data/product_category_name_translation.csv")
+orders = pd.read_csv("./data/olist_orders_dataset.csv")
+order_payments = pd.read_csv("./data/olist_order_payments_dataset.csv")
+
+
+datasets = [customers, sellers, reviews, items, products, geolocation, category_name_translation, orders, order_payments]
+
+df = orders.merge(items, on="order_id").merge(products, on = "product_id").merge(sellers, on="seller_id").merge(customers, on="customer_id")
+
+df.info()
+
+# +
+#date_cols = [order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "order_delivered_customer_date"]
+
+df["order_delivered_carrier_date"] = pd.to_datetime(df["order_delivered_carrier_date"])
+df["order_estimated_delivery_date"] = pd.to_datetime(df["order_estimated_delivery_date"])
+df["order_purchase_timestamp"] = pd.to_datetime(df["order_purchase_timestamp"])
+df["order_delivered_customer_date"] = pd.to_datetime(df["order_delivered_customer_date"])
+
+df['expected_delivery_timedelta'] = df['order_estimated_delivery_date']- df["order_purchase_timestamp"]
+df['delivery_time'] = df["order_delivered_customer_date"] - df['order_purchase_timestamp']
+
+df['product_volume'] = df["product_length_cm"] * df["product_height_cm"] * df["product_width_cm"]
+
+df["same_city"] = df["customer_city"] == df["seller_city"]
+df["same_state"] = df["customer_state"] == df["seller_state"]
+
+# +
+unique_orders_count = df.groupby(["customer_id"])["order_id"].count() # Count unique orders
+nof_moest_popular_sales = df.groupby(["customer_id"])["seller_id"].agg(lambda x:x.value_counts()[0]) # Number of orders from most popular sellers
+max_sale = df.groupby(["customer_id"])["price"].max() # Most money spent on single order
+median_sale = df.groupby(["customer_id"])["price"].median() # Median money spent on orders
+sum_sale = df.groupby(["customer_id"])["price"].sum() # Median money spent on orders
+
+median_volume = df.groupby(["customer_id"])["product_volume"].median() # Median volume of products in orders
+max_volume = df.groupby(["customer_id"])["product_volume"].max() # Median money spent on orders
+
+average_delivery_time = df.groupby(["customer_id"])["delivery_time"].max() # Average delivery time
+average_expected_delivery_time = df.groupby(["customer_id"])["expected_delivery_timedelta"].max() # Average delivery time
+
+
+unique_orders_count.name = "unique_orders_count"
+nof_moest_popular_sales.name = "nof_moest_popular_sales"
+max_sale.name = "max_sale"
+median_sale.name = "median_sale"
+sum_sale.name = "sum_sale"
+median_volume.name = "median_volume"
+average_delivery_time.name = "average_delivery_time"
+average_expected_delivery_time.name = "average_expected_delivery_time"
+# -
+
+customers2 = customers.set_index("customer_id")
+
+customers2
+
+df2 = customers2.join([unique_orders_count, nof_moest_popular_sales, max_sale, median_sale, sum_sale, median_volume, average_delivery_time,average_expected_delivery_time],  how="outer")
+
+df2[df2["unique_orders_count"] > 1]
+
+cat_attributes = ["customer_city", "customer_state"]
+num_attributes = ["unique_orders_count", "nof_moest_popular_sales", "max_sale", "median_sale", "sum_sale", "median_volume", "average_delivery_time","average_expected_delivery_time"]
+
+df2['average_expected_delivery_time'] = df2['average_expected_delivery_time'].dt.total_seconds()
+df2['average_delivery_time'] = df2['average_delivery_time'].dt.total_seconds()
+
+df2.fillna(0)
+
+# +
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+
+pipeline = ColumnTransformer([
+        ('num', StandardScaler(), num_attributes),
+        ('cat', OneHotEncoder(), cat_attributes),
+])
+df_prepared = pipeline.fit_transform(df2.fillna(0))
+df_prepared
+
+# +
+
+
+df3 = df_prepared.toarray()
+# -
+
+import umap
+
+import umap.plot
+
+mapper = umap.UMAP().fit(df3)
+
+umap.plot.points(mapper)
+
+mapper2 = umap.UMAP(densmap=True).fit(df3[np.random.choice(df3.shape[0], 10000, replace=False)])
+
+
+umap.plot.points(mapper2)
+
+df3.data
+
+df3.shape
+
+df3
+
+# +
+from yellowbrick.cluster import KElbowVisualizer
+model = KMeans()
+
+visualizer = KElbowVisualizer(model, k=(4,10), timings=True)
+visualizer.fit(df3)
+visualizer.show()
+# -
+
+from sklearn.cluster import KMeans
+X = df3
+kmeans = KMeans(n_clusters=6, random_state=0, copy_x=False, precompute_distances=False).fit(X)
+
+kmeans.labels_
+
+umap.plot.points(mapper, labels=kmeans.labels_)
+
+
