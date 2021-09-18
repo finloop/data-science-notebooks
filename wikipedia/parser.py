a38fb0fcf8345a7614e333e064a2ff06cf1e3c3b
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-09-18 20:57:03 +0200

    add get_graph

--- wikipedia/parser.py
+++ wikipedia/parser.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib3
 import time
+import networkx as nx
 
 
 def get_links_from_wiki(soup, n=5, prefix="https://en.wikipedia.org"):
@@ -15,26 +16,47 @@ def get_links_from_wiki(soup, n=5, prefix="https://en.wikipedia.org"):
     return arr
 
 
-def crawl(pool: urllib3.PoolManager, url, deep=1, sleep_time=0.5):
+def crawl(pool: urllib3.PoolManager, url, deep=1, sleep_time=0.5, n=5, prefix="https://en.wikipedia.org"):
     time.sleep(sleep_time)
     site = pool.request("GET", url)
     soup = BeautifulSoup(site.data, parser="lxml")
     if deep > 0:
         return (
             url,
-            [crawl(pool, url_, deep - 1) for url_ in get_links_from_wiki(soup=soup)],
+            [crawl(pool, url_, deep - 1) for url_ in get_links_from_wiki(
+                soup=soup, n=n, prefix=prefix)],
         )
-    return (url, get_links_from_wiki(soup=soup))
+    return url, get_links_from_wiki(soup=soup, n=n, prefix=prefix)
 
 
-def edges(item):
+def get_edges(item):
     edges = []
     url, elements = item
     if isinstance(elements, list):
         for element in elements:
             if isinstance(element, str):
-                edges.append((url, element))
+                edges.append((url.split("/")[-1], element.split("/")[-1]))
             else:
-                edges.append((url, element[0]))
-                edges += edges(element)
+                edges.append((url.split("/")[-1], element[0].split("/")[-1]))
+                edges += get_edges(element)
     return edges
+
+
+def get_nodes(edges):
+    nodes = []
+    for edge in edges:
+        nodes += list(edge)
+    return nodes
+
+
+def get_graph(pool,url, deep=1, sleep_time=0.5, n=5,
+              prefix="https://en.wikipedia.org" ):
+    tree = crawl(pool=pool, url=url, deep=deep, sleep_time=sleep_time, n=n,
+                 prefix=prefix)
+    edges = get_edges(tree)
+    nodes = get_nodes(edges)
+
+    graph = nx.DiGraph()
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+    return graph
