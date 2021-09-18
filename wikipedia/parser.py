85c4b1c962fc0af711e5f218ff911ef2552397ad
Piotr Krawiec <piotr.krawiec23@gmail.com>
2021-09-18 21:26:41 +0200

    add docs

--- wikipedia/parser.py
+++ wikipedia/parser.py
@@ -5,6 +5,23 @@ import networkx as nx
 
 
 def get_links_from_wiki(soup, n=5, prefix="https://en.wikipedia.org"):
+    """
+    Extracts `n` first links from wikipedia articles and adds `prefix` to
+    internal links.
+
+    Parameters
+    ----------
+    soup : BeautifulSoup
+        Wikipedia page
+    n : int
+        Number of links to return
+    prefix : str, default="https://en.wikipedia.org""
+        Site prefix
+    Returns
+    -------
+    list
+        List of links
+    """
     arr = []
     for i, a in enumerate(
         soup.find("div", class_="mw-parser-output").find("p").find_all("a", href=True)
@@ -17,6 +34,30 @@ def get_links_from_wiki(soup, n=5, prefix="https://en.wikipedia.org"):
 
 
 def crawl(pool: urllib3.PoolManager, url, deep=1, sleep_time=0.5, n=5, prefix="https://en.wikipedia.org"):
+    """
+    Crawls given Wikipedia `url` (article) with depth `deep`. For each page
+    extracts `n` urls.
+
+    Parameters
+    ----------
+    pool : urllib3.PoolManager
+        Request pool
+    url : str
+        Link to wikipedia article
+    deep : int
+        Depth of crawl
+    sleep_time : float
+        Sleep time between requests.
+    n : int
+        Number of links to return
+    prefix : str, default="https://en.wikipedia.org""
+        Site prefix
+
+    Returns
+    -------
+    tuple
+        Tuple of url, list
+    """
     time.sleep(sleep_time)
     site = pool.request("GET", url)
     soup = BeautifulSoup(site.data, parser="lxml")
@@ -29,9 +70,28 @@ def crawl(pool: urllib3.PoolManager, url, deep=1, sleep_time=0.5, n=5, prefix="h
     return url, get_links_from_wiki(soup=soup, n=n, prefix=prefix)
 
 
-def get_edges(item):
+def get_edges(tree):
+    """
+    Get edges from url tree. Where url tree is tuple of (url, list of tuples(
+    url, list)) etc.
+
+    Example tree:
+    (url, [(url1, [...]),
+          (url2, [...]).
+    ])
+
+    Parameters
+    ----------
+    tree : tuple
+        Tree of urls.
+
+    Returns
+    -------
+    list
+        List of tuples (source page, end page).
+    """
     edges = []
-    url, elements = item
+    url, elements = tree
     if isinstance(elements, list):
         for element in elements:
             if isinstance(element, str):
@@ -43,14 +103,48 @@ def get_edges(item):
 
 
 def get_nodes(edges):
+    """
+    Get nodes from list of edges.
+
+    Parameters
+    ----------
+    edges : list
+         List of tuples (source page, end page).
+    Returns
+    -------
+    list
+        nodes, not unique
+    """
     nodes = []
     for edge in edges:
         nodes += list(edge)
     return nodes
 
 
-def get_graph(pool,url, deep=1, sleep_time=0.5, n=5,
-              prefix="https://en.wikipedia.org" ):
+def get_graph(pool, url, deep=1, sleep_time=0.5, n=5,
+              prefix="https://en.wikipedia.org"):
+    """
+    Generates link graph for given Wikipedia article.
+
+    Parameters
+    ----------
+    pool : urllib3.PoolManager
+        Request pool
+    url : str
+        Link to wikipedia article
+    deep : int
+        Depth of crawl
+    sleep_time : float
+        Sleep time between requests.
+    n : int
+        Number of links to return
+    prefix : str, default="https://en.wikipedia.org""
+        Site prefix
+    Returns
+    -------
+    nx.DiGraph
+        Graph of url.
+    """
     tree = crawl(pool=pool, url=url, deep=deep, sleep_time=sleep_time, n=n,
                  prefix=prefix)
     edges = get_edges(tree)
